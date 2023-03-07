3d3b41815e2ae01b819acafad355f2d2643aa3b1
Kwizera Claude <71407602+kclaudeeager@users.noreply.github.com>
2023-03-07 14:37:04 +0200

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Claude KWIZERA on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
