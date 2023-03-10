54c865b73ed1b84d003ffc61db584bddef879eb9
Kwizera Claude <71407602+kclaudeeager@users.noreply.github.com>
2023-03-10 12:09:03 +0200

    Created a login page

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -6,8 +6,93 @@
 //
 
 import SwiftUI
+struct LoginFormView: View {
+    @State var userEmail: String = ""
+    @State var password: String = ""
+    enum Field: Hashable {
+        case emailField
+        case passwordField
+    }
+    @FocusState private var focusedField: Field?
+    @State private var isEmailValid = true
+    var body: some View {
+        ZStack {
+            Color(UIColor(red: 240/255, green: 238/255, blue: 238/255, alpha: 1.0))
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                Text("Restaurant Login")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 50)
+                Image(systemName: "building.fill")
+                    .font(.system(size: 60))
+                    .foregroundColor(.black)
+                    .shadow(color: .black, radius: 10, x: 0, y: 0)
+                VStack(spacing: 20) {
+                    TextField("Enter your email", text: $userEmail)
+                        .foregroundColor(.black)
+                        .padding(10)
+                        .background(Color.white)
+                        .cornerRadius(20)
+                        .padding(.horizontal, 30)
+                        .focused($focusedField, equals: .emailField)
+                        .onChange(of: userEmail) { newValue in
+                            validateEmail()
+                        }
+                    if !isEmailValid {
+                        Text("Please enter a valid email address")
+                            .foregroundColor(.red)
+                            .padding(.horizontal, 30)
+                    }
+                    SecureField("Enter your password", text: $password)
+                        .foregroundColor(.black)
+                        .padding(10)
+                        .background(Color.white)
+                        .cornerRadius(20)
+                        .padding(.horizontal, 30)
+                        .focused($focusedField, equals: .passwordField)
+                }
+                Button(action: {
+                    if userEmail.isEmpty {
+                        focusedField = .emailField
+                    }
+                    else if password.isEmpty {
+                        focusedField = .passwordField
+                    }
+                    else {
+                        print("Button tapped")
+                    }
+                }) {
+                    Text("Login")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding(.vertical, 15)
+                        .frame(width: 220)
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                        .shadow(color: .black, radius: 5, x: 0, y: 0)
+                }
+                Spacer()
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+    func validateEmail() {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        isEmailValid = emailPredicate.evaluate(with: userEmail)
+    }
+}
 
-struct ContentView: View {
+
+
+
+
+struct OtherPart:View{
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -48,8 +133,16 @@ struct ContentView: View {
     }
 }
 
+
+struct ContentView: View {
+    var body: some View {
+        LoginFormView()
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+    
+        LoginFormView()
     }
 }
